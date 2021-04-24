0b1ee4e2e92513907eb2be05f270481eaae11445
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-24 01:31:16 -0400

    Cambio de nombre de archivo Aplication del package principal

--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/crud/EvaluacionFinalMod5FrontAplication.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/crud/EvaluacionFinalMod5FrontAplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Grupal7Application {
+public class EvaluacionFinalMod5FrontAplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Grupal7Application.class, args);
+		SpringApplication.run(EvaluacionFinalMod5FrontAplication.class, args);
 	}
 
 }
--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/crud/ServletInitializer.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/crud/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Grupal7Application.class);
+		return application.sources(EvaluacionFinalMod5FrontAplication.class);
 	}
 
 }
