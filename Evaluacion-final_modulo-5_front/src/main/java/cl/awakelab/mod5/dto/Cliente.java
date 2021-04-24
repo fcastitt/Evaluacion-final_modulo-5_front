0a04f6d9f0299e11639ec1a315b5d1eaae6e78f8
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-24 02:32:27 -0400

    Creo dto Cliente

---
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/dto/Cliente.java
@@ -0,0 +1,16 @@
+package cl.awakelab.mod5.dto;
+
+public class Cliente {
+
+	
+	private Integer rutCliente;
+	private String cliNombres;
+	private String cliApellidos;
+	private String cliTelefono;
+	private String cliAfp;
+	private String cliSistemaSalud;
+	private String cliDireccion;
+	private String cliComuna;
+	private String cliEdad;
+	private Integer usuariosURun;
+}
