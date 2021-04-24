0a287c83779376949a3ea9ad7b6721eb26f7de50
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-24 04:00:14 -0400

    implementacion seguridad 1

--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/seguridad/AutorizacionServicio.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/seguridad/AutorizacionServicio.java
@@ -1,5 +1,16 @@
 package cl.awakelab.mod5.seguridad;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.awakelab.mod5.dto.Usuarios;
+import cl.awakelab.mod5.servicio.InterfasServicios;
+
+@Service
 public class AutorizacionServicio {
+	
+	@Autowired
+	InterfasServicios<Usuarios> IntUsu;
+	
 
 }
