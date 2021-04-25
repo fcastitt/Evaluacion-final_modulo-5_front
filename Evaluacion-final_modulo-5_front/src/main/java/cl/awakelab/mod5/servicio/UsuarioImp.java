cd0104574570cc4a0359c511163fd659a9886370
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-25 00:13:21 -0400

    Instacie la api buscarporrun

--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/UsuarioImp.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/UsuarioImp.java
@@ -17,7 +17,7 @@ public class UsuarioImp implements InterfasServicios<Usuarios>{
 
 	//private static final String APIUSUARIO2 = "http://localhost:8889/api/v1/usuario2";
 	//private static final String APICREARUSUARIO = "http://localhost:8889/api/v1/usuario2/crear";
-	//private static final String APIBUSCARPORRUN = "http://localhost:8889/api/v1/usuario2/buscar/{idUsuario}";
+	private static final String APIBUSCARPORRUN = "http://localhost:8889/api/v1/usuarios/buscar/{idUsuario}";
 	
 	@Autowired
 	RestTemplate restTemp;
