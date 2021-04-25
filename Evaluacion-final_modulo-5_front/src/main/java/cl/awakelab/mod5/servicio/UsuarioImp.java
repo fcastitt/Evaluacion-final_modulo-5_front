2487ba5fe851494dc738d6900acff3c86461311b
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-24 23:33:29 -0400

    metodo buscarporid comentado

--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/UsuarioImp.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/UsuarioImp.java
@@ -1,11 +1,30 @@
 package cl.awakelab.mod5.servicio;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import cl.awakelab.mod5.dto.Usuarios;
 
+@Service
 public class UsuarioImp implements InterfasServicios<Usuarios>{
 
+	//private static final String APIUSUARIO2 = "http://localhost:8889/api/v1/usuario2";
+	//private static final String APICREARUSUARIO = "http://localhost:8889/api/v1/usuario2/crear";
+	//private static final String APIBUSCARPORRUN = "http://localhost:8889/api/v1/usuario2/buscar/{idUsuario}";
+	
+	@Autowired
+	RestTemplate restTemp;
+	
+	@Autowired
+	BCryptPasswordEncoder bCPE;
+	
 	@Override
 	public List<Usuarios> listar() {
 		// TODO Auto-generated method stub
@@ -32,8 +51,13 @@ public class UsuarioImp implements InterfasServicios<Usuarios>{
 
 	@Override
 	public Usuarios buscarPorId(String runUsuario) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		Map<String, String> maparun = new HashMap<String, String>();
+		maparun.put("idUsuario", runUsuario);
+		
+		//ResponseEntity<Usuarios> responseUsuario = restTemp.getForEntity(APIBUSCARPORRUN, Usuarios.class, maparun);
+		
+		return null;//responseUsuario.getBody();
 	}
 
 }
