4d9185d0c63a32a02567822b152c8e212e9d4982
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-24 23:24:11 -0400

    Agrego UsuarioImp

---
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/UsuarioImp.java
@@ -0,0 +1,39 @@
+package cl.awakelab.mod5.servicio;
+
+import java.util.List;
+
+import cl.awakelab.mod5.dto.Usuarios;
+
+public class UsuarioImp implements InterfasServicios<Usuarios>{
+
+	@Override
+	public List<Usuarios> listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuarios crear(Usuarios dato) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void borrar(Usuarios dato) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Usuarios modificar(Usuarios dato) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuarios buscarPorId(String runUsuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
