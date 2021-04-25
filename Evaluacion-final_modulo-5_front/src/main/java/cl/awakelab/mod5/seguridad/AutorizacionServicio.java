bba44d94b80b3ef5405f394633ab0fb61f6ad02a
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-24 23:23:50 -0400

    agrgo metodo de user detail

--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/seguridad/AutorizacionServicio.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/seguridad/AutorizacionServicio.java
@@ -1,16 +1,25 @@
 package cl.awakelab.mod5.seguridad;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import cl.awakelab.mod5.dto.Usuarios;
 import cl.awakelab.mod5.servicio.InterfasServicios;
 
 @Service
-public class AutorizacionServicio {
+public class AutorizacionServicio implements UserDetailsService{
 	
 	@Autowired
 	InterfasServicios<Usuarios> IntUsu;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 
 }
