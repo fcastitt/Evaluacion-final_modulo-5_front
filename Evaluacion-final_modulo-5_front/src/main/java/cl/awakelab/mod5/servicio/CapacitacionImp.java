1b149744cfd744acf6c46988bcc7a5fda217bd19
Hugo <Hugo@DESKTOP-BS9RQQ1>
2021-04-26 14:38:52 -0400

    Agrego metodos listar y crea en Implementaicon de Capacitacion

--- Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/CapacitacionImp.java
+++ Evaluacion-final_modulo-5_front/src/main/java/cl/awakelab/mod5/servicio/CapacitacionImp.java
@@ -3,6 +3,11 @@ package cl.awakelab.mod5.servicio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,19 +16,37 @@ import cl.awakelab.mod5.dto.Capacitacion;
 @Service
 public class CapacitacionImp implements InterfasServicios<Capacitacion> {
 
-	// falta agregar implemetar apis
+	private static final String APILISTARCAPACITACION = "http://localhost:8889/api/v1/capacitacion";
+	private static final String APICREARCAPACITACION = "http://localhost:8889/api/v1/capacitacion/crear";
 	
 	@Autowired
-	RestTemplate retTemp;
+	RestTemplate restTemp;
+	
+	@Autowired
+	InterfasServicios<Capacitacion> IntCapacitacion;
+
 	
 	@Override
 	public List<Capacitacion> listar() {
-		return null;
+			
+		HttpHeaders headers = new HttpHeaders();
+		
+		HttpEntity<Capacitacion> entityCapacitacion = new HttpEntity<Capacitacion>(headers);
+		
+		ResponseEntity<List<Capacitacion>> responseUsuarios = restTemp.exchange(APILISTARCAPACITACION, HttpMethod.GET, entityCapacitacion, new ParameterizedTypeReference<List<Capacitacion>>() {
+		});
+		
+		return responseUsuarios.getBody();
+		
 	}
 
+	
 	@Override
 	public Capacitacion crear(Capacitacion dato) {
-		return null;
+		
+		ResponseEntity<Capacitacion> responseUsuario = restTemp.postForEntity(APICREARCAPACITACION, dato, Capacitacion.class);
+		
+		return responseUsuario.getBody();
 	}
 
 	@Override
